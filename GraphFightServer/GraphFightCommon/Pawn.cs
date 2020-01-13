2f50007d746ee29eea61c78e3d2b2df57b6f5968
magico13 <magico1313@gmail.com>
2020-01-12 22:14:45 -0500

    Create pawn class

---
+++ GraphFightServer/GraphFightCommon/Pawn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphFightCommon
+{
+    public class Pawn
+    {
+        public int Health { get; set; }
+        public int MaxHealth { get; set; }
+        public int Movement { get; set; }
+        public int MaxMovement { get; set; }
+
+        public Pawn(int health = 1, int move = 3)
+        {
+            MaxHealth = Health = health;
+            MaxMovement = Movement = move;
+        }
+    }
+}
---
+++ GraphFightServer/GraphFightCommonTests/PawnTests.cs
@@ -0,0 +1,28 @@
+﻿using GraphFightCommon;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphFightCommonTests
+{
+    [TestFixture]
+    public class PawnTests
+    {
+        [Test]
+        public void PawnHasHealth()
+        {
+            Pawn pawn = new Pawn(health: 3);
+            Assert.That(pawn.MaxHealth, Is.EqualTo(3));
+            Assert.That(pawn.Health, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void PawnHasMovement()
+        {
+            Pawn pawn = new Pawn(move: 2);
+            Assert.That(pawn.MaxMovement, Is.EqualTo(2));
+            Assert.That(pawn.Movement, Is.EqualTo(2));
+        }
+    }
+}
